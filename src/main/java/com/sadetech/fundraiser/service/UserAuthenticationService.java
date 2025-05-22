7b77eb06ae93ad1726afdbb18305c9aad8ef1f93
Gokulkrish17 <gokulgokul868@gmail.com>
2025-05-22 17:27:43 +0530

    Added bean for cors configuration in security config

--- src/main/java/com/sadetech/fundraiser/service/UserAuthenticationService.java
+++ src/main/java/com/sadetech/fundraiser/service/UserAuthenticationService.java
@@ -17,7 +17,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.FileNotFoundException;
 import java.time.LocalDateTime;
 import java.util.*;
-import java.util.concurrent.TimeoutException;
 
 @Service
 public class UserAuthenticationService {
